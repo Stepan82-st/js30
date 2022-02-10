62d868939bc40e73e53c252d411f8c05bbef7be7
Stepan Bryl <bryl.step@tut.by>
2022-02-10 21:11:25 +0300

    feat: random-jokes

--- random-jokes/index.js
+++ random-jokes/index.js
@@ -1,10 +1,28 @@
-const url = 'https://type.fit/api/quotes';
+const url = 'https://api.icndb.com/jokes/random';
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
 
-
+console.log(input);
 
   async function getData() {
+    const quotes = 'quotes.json';
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
+    const text = document.createTextNode(data);
+    return text;
   }
-  getData();
+ console.log(getData());
+
+  function getZapros(){
+    const $p1 = document.createElement('p');
+    $p1.textContent = getData();
+    // вставим элемент $p1 в начало $message
+    input.prepend($p1); 
+  }
+  
+  btn.addEventListener('click', () => {
+  
+    let pElem = document.createElement('p');
+    pElem.textContent = getZapros().map(item => item.value);
+    document.body.appendChild(pElem);
+  });
