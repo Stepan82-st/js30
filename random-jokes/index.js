e1c8500ecf8673e8fb05b23d4d11d276d4d477c6
Stepan Bryl <bryl.step@tut.by>
2022-02-12 20:59:48 +0300

    feat: random-jokes

--- random-jokes/index.js
+++ random-jokes/index.js
@@ -2,27 +2,21 @@ const url = 'https://api.icndb.com/jokes/random';
 const input = document.querySelector('.input');
 const btn = document.querySelector('.btn');
 
-console.log(input);
+ 
+ btn.addEventListener('click', async () => {
+  try {
+    const response = await fetch('quotes.json');
+    const result = await response.json();     
+    
+         let i = Math.floor(Math.random() * result.length)
+        input.textContent = result[i].text + " - "+ result[i].author;
+      console.log(input)
+    }
+       catch(err){
+         console.log(err)
+       }
 
-  async function getData() {
-    const quotes = 'quotes.json';
-    const res = await fetch(url);
-    const data = await res.json();
-    const text = document.createTextNode(data);
-    return text;
-  }
- console.log(getData());
+   });  
 
-  function getZapros(){
-    const $p1 = document.createElement('p');
-    $p1.textContent = getData();
-    // вставим элемент $p1 в начало $message
-    input.prepend($p1); 
-  }
   
-  btn.addEventListener('click', () => {
   
-    let pElem = document.createElement('p');
-    pElem.textContent = getZapros().map(item => item.value);
-    document.body.appendChild(pElem);
-  });
